a75292735b7f72bf7ad5939be4fdf46cd314fee5
Leanid2000 <banrulit100000@gmail.com>
2025-09-01 02:51:15 +0300

    fix: fix Card

--- src/components/List/Card/Card.tsx
+++ src/components/List/Card/Card.tsx
@@ -1,3 +1,4 @@
+import { memo } from 'react';
 import type {
   Data,
   Parameters,
@@ -5,30 +6,32 @@ import type {
 } from '../../../interfaces/interfaces';
 import Table from './Table/Table';
 
-export const Card = ({
-  data,
-  checkedParams,
-  year,
-  country,
-}: {
-  data: Response;
-  checkedParams: Parameters[];
-  year: number;
-  country: string;
-}) => {
-  const countryData: Data[] = data[country].data;
-  const position = countryData.findIndex((elem) => elem.year === year);
-  return (
-    <div
-      key={country}
-      className="flex flex-col justify-center items-center border-b-5 w-150 pb-5"
-    >
-      <p>Name: {country}</p>
-      <p>ISO: {data[country].iso_code}</p>
-      <p>Population: {countryData[position]?.population || 'N/A'}</p>
-      {position !== -1 && (
-        <Table data={countryData[position]} checkedParams={checkedParams} />
-      )}
-    </div>
-  );
-};
+export const Card = memo(
+  ({
+    data,
+    checkedParams,
+    year,
+    country,
+  }: {
+    data: Response;
+    checkedParams: Parameters[];
+    year: number;
+    country: string;
+  }) => {
+    const countryData: Data[] = data[country].data;
+    const position = countryData.findIndex((elem) => elem.year === year);
+    return (
+      <div
+        key={country}
+        className="flex flex-col justify-center items-center border-b-5 w-150 pb-5"
+      >
+        <p>Name: {country}</p>
+        <p>ISO: {data[country].iso_code}</p>
+        <p>Population: {countryData[position]?.population || 'N/A'}</p>
+        {position !== -1 && (
+          <Table data={countryData[position]} checkedParams={checkedParams} />
+        )}
+      </div>
+    );
+  }
+);
